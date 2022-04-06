a96e9313bff7ddf411d1a6179224bb82e870d6c0
kimjinsoo <1004kjs_@naver.com>
2022-04-06 17:36:43 +0900

    <chore>: update configuration.js
    - updte confguration -> google_user => email_user, google_pass => email_pass

--- configuration.js
+++ configuration.js
@@ -8,9 +8,11 @@ const database_url = process.env.DATABASE_URL;
 const twilio_accountsid = process.env.TWILIO_ACCOUNTSID;
 const twilio_authtoken = process.env.TWILIO_AUTHTOKEN;
 const secret = process.env.SECRET;
-const google_user = process.env.GOOGLE_USER;
-const google_pass = process.env.GOOGLE_PASS;
+const email_user = process.env.EMAIL_USER;
+const email_pass = process.env.EMAIL_PASS;
 const slack_api_token = process.env.SLACK_API_TOKEN;
+const s3_access_key_id = process.env.S3_ACCESS_KEY_ID;
+const s3_secret_access_key = process.env.S3_SECRET_ACCESS_KEY;
 
 export default () => ({
     port,
@@ -20,7 +22,9 @@ export default () => ({
     twilio_accountsid,
     twilio_authtoken,
     secret,
-    google_user,
-    google_pass,
+    email_user,
+    email_pass,
     slack_api_token,
+    s3_access_key_id,
+    s3_secret_access_key,
 });

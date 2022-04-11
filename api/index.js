8233c24b8d7ffee62b04b316c5e8895a1d3d3648
kimjinsoo <1004kjs_@naver.com>
2022-04-11 13:32:23 +0900

    <feat>: update api/index.js
    - update api/index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import user from "./controllers/user.js";
 import team from "./controllers/team.js";
+import profile from "./controllers/profile.js";
 
 const router = express.Router();
 
 router.use("/user", user);
 router.use("/team", team);
+router.use("/profile", profile);
 
 export default router;

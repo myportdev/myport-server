835564eb765e8cff889cdd0417367f6a71e08249
kimjinsoo <1004kjs_@naver.com>
2022-04-11 13:45:47 +0900

    <chore>: update api/index.js
    - update api/index.js

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import user from "./controllers/user.js";
 import team from "./controllers/team.js";
 import profile from "./controllers/profile.js";
+import home from "./controllers/home.js";
 
 const router = express.Router();
 
 router.use("/user", user);
 router.use("/team", team);
 router.use("/profile", profile);
+router.use("/home", home);
 
 export default router;

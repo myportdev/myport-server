307875152550feadfd34bd708987cb9a5560d9c3
kimjinsoo <1004kjs_@naver.com>
2022-04-11 13:19:19 +0900

    <feat>: feature/home
    - get home

---
+++ api/controllers/home.js
@@ -0,0 +1,63 @@
+import { Router } from "express";
+import auth_token from "../../middlewares/auth_token.js";
+import home_service from "../services/home.js";
+import date_difference from "../../modules/date_difference.js";
+
+const router = Router();
+
+router.get("/", auth_token, async (req, res, next) => {
+    try {
+        const user_id = res.locals.user_id;
+
+        const user = await home_service.get_user(user_id);
+        const same_interest_users = await home_service.get_same_interest_user(user);
+
+        const contests = await home_service.get_contests();
+        let contests_data = [];
+        if (contests) {
+            for (const contest of contests) {
+                const str_date = contest.end_date;
+                let d_day = date_difference(str_date);
+                contests_data.push({
+                    _id: contest.id,
+                    image_url_thumbnail: contest.image_url_thumbnail,
+                    title: contest.title,
+                    dday: d_day,
+                    relation_category: contest.relation_category,
+                    founder: contest.founder ? contest.founder : "",
+                });
+            }
+        }
+        const teams = await home_service.find_recruiting_list_team(user);
+        let teams_data = [];
+        if (teams) {
+            for (const team of teams) {
+                teams_data.push({
+                    _id: team.id,
+                    recruiting: team.recruiting,
+                    team_name: team.team_name,
+                    team_image_url: team.team_image_url,
+                    team_image_url: team.team_image_url ? team.team_image_url : "",
+                    relation_contest: team.relation_contest ? team.relation_contest : "",
+                    relation_extracurricular: team.relation_extracurricular ? team.relation_extracurricular : "",
+                    team_members: team.team_members,
+                    team_type: team.team_type,
+                });
+            }
+        }
+
+        const profile = await home_service.get_profile(user_id);
+        const portfolio = await home_service.get_portfolio(user_id);
+
+        res.status(200).json({
+            users: same_interest_users,
+            contests: contests_data,
+            teams: teams_data,
+            completion_rate: profile.completion_rate + portfolio.completion_rate,
+        });
+    } catch (error) {
+        next(error);
+    }
+});
+
+export default router;
---
+++ api/services/home.js
@@ -0,0 +1,73 @@
+import Portfolio from "../../models/Portfolio.js";
+import Profile from "../../models/Profile.js";
+import User from "../../models/User.js";
+import Contest from "../../models/Contest.js";
+import Team from "../../models/Team.js";
+
+const home_service = {
+    get_user: async (user_id) => {
+        const user = await User.findById({ _id: user_id }).exec();
+        return user;
+    },
+
+    get_same_interest_user: async (user) => {
+        let users = [];
+        for (const index in user.interest) {
+            users = users.concat(
+                await User.find({ interest: { $in: [user.interest[index]] } })
+                    .limit(10)
+                    .select("name image_url_medium")
+            );
+        }
+        const filter_users = users.filter((item1, index1) => {
+            if (item1.id != user.id) {
+                return (
+                    users.findIndex((item2, index2) => {
+                        return item1.id === item2.id;
+                    }) === index1
+                );
+            }
+        });
+        return filter_users;
+    },
+
+    get_profile: async (user_id) => {
+        const profile = await Profile.findOne({ user: user_id }).exec();
+        return profile;
+    },
+
+    get_portfolio: async (user_id) => {
+        const portfolio = await Portfolio.findOne({ user: user_id }).exec();
+        return portfolio;
+    },
+
+    get_contests: async () => {
+        const contests = await Contest.find()
+            .populate("relation_category", "interest_name")
+            .select("_id image_url_thumbnail title relation_category end_date founder")
+            .limit(15)
+            .sort({ start_date: 1 })
+            .exec();
+        return contests;
+    },
+
+    find_recruiting_list_team: async (user) => {
+        const team = await Team.find()
+            .nor([{ team_leader: user }, { team_members: { $in: [user] } }, { waiting_for_support: { $in: [user] } }])
+            .select("team_members relation_contest relation_extracurricular team_name team_type team_image_url recruiting")
+            .populate({
+                path: "relation_contest",
+                select: "image_url_thumbnail title",
+            })
+            .populate({
+                path: "relation_extracurricular",
+                select: "image_url_thumbnail title",
+            })
+            .sort({ create_date: -1 })
+            .limit(6)
+            .exec();
+        return team;
+    },
+};
+
+export default home_service;
